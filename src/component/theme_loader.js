6bd53367846d2592903faec2a765310863de48d1
Niklas Hoesl <nik.hoesl@hotmail.com>
2019-06-23 17:56:07 +0200

    ADD Ladepreise whitelabel

--- src/component/theme_loader.js
+++ src/component/theme_loader.js
@@ -4,25 +4,28 @@ require('jsrender')($);
 export default class ThemeLoader {
   constructor(translation) {
     this.translation = translation;
-    this.defaultTheme = "default";
+    this.defaultTheme = "plugchecker";
     this.themes = {
       [this.defaultTheme] : {
-        titleBarHtml: "<img id=\"logo\" src=\"img/logo.png\"/>",
+        titleBarHtml: "<img id=\"logo\" src=\"themes/plugchecker/logo.png\"/>",
         title: translation.get("title"),
         favicon: "img/favicon-32x32.png",
-        themeColor: "#3498db"
+        themeColor: "#3498db",
+        appleTouchIcon: "/img/logos/apple-touch-icon.png"
       },
       emc: {
-        titleBarHtml: `<img id=\"logo\" src=\"img/emc.png\"/><span class=\"title\">${translation.get("themeTitle")}</span>`,
-        favicon: "img/emc.png",
+        titleBarHtml: `<img id=\"logo\" src=\"themes/emc/logo.png\"/><img id=\"logoText\" src=\"themes/emc/logo_text.png\"/>`,
+        favicon: "themes/emc/logo.png",
         name: `EMC ${translation.get("themeTitle")}`,
-        themeColor: "#8fbf22"
+        themeColor: "#8fbf22",
+        appleTouchIcon: "themes/emc/logo.png"
       },
       nissan: {
-        titleBarHtml: `<img id=\"logo\" src=\"img/nissan.png\"/><span class=\"title\">${translation.get("themeTitle")}</span>`,
-        favicon: "img/nissan.png",
+        titleBarHtml: `<img id=\"logo\" src=\"themes/nissan/logo.png\"/><span class=\"title\">${translation.get("themeTitle")}</span>`,
+        favicon: "themes/nissan/logo.png",
         name: `Nissan ${translation.get("themeTitle")}`,
-        themeColor: "#c3002f"
+        themeColor: "#c3002f",
+        appleTouchIcon: "/img/logos/apple-touch-icon.png"
       }
     }
   }
@@ -39,16 +42,16 @@ export default class ThemeLoader {
     // Set CSS
     var newSS=document.createElement('link');
     newSS.rel='stylesheet';
-    newSS.href=`css/themes/${themeId}.css`;
+    newSS.href=`themes/${themeId}/style.css`;
     document.getElementsByTagName("head")[0].appendChild(newSS);
 
     // Title Bar
     document.getElementById("logo-container").innerHTML = theme.titleBarHtml;
 
-    
-
     // Favicon
-    document.getElementById("pc-icon").setAttribute("href",theme.favicon);
+    document.getElementById("metaIcon").setAttribute("href",theme.favicon);
+    document.getElementById("metaAppleIcon").setAttribute("href",theme.appleTouchIcon);
+    document.getElementById("metaManifest").setAttribute("href",`themes/${themeId}/site.webmanifest`);
 
     // Theme Color
     document.getElementById("theme-color").setAttribute("content",theme.themeColor);
@@ -59,7 +62,7 @@ export default class ThemeLoader {
     }
     else {
       document.getElementById("theme-name").innerText = theme.name;
-      document.getElementsByTagName("title")[0].innerText = theme.name + `${this.translation.get("poweredBy")} Plugchecker.com`;
+      document.getElementsByTagName("title")[0].innerText = `${theme.name} ${this.translation.get("poweredBy")} Chargeprice`;
     }
   }
 
