58ff940b08385a18fbaa575717530232cb49d8e6
Niklas Hösl <niklas.hoesl@runtastic.com>
2021-03-13 21:38:39 +0100

    ADD margin below feedback

--- src/component/pricesSidebar.js
+++ src/component/pricesSidebar.js
@@ -17,7 +17,7 @@ export default class PricesSidebar extends ViewBase {
       <div id="priceInfo" class="w3-container"></div>
 
       <div id="prices"></div> 
-      <div id="priceFeedback" class="w3-margin-top w3-container"></div>
+      <div id="priceFeedback" class="w3-margin-top w3-container w3-margin-bottom"></div>
     `;
   }
 
